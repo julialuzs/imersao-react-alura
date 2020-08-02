f4c99982581c1db1f83de4a377a5293d12537b6b
Julia Luz dos Santos <julia.15081@gmail.com>
2020-08-02 01:56:06 -0300

    fix: style

--- src/components/carousel/styles.js
+++ src/components/carousel/styles.js
@@ -2,15 +2,12 @@ import styled from 'styled-components';
 
 export const Title = styled.h3`
   font-style: normal;
-  font-weight: normal;
-  font-size: 35px;
+  font-weight: 100;
+  font-size: 22px;
   line-height: 1;
   margin-bottom: 16px;
   display: inline-block;
-  /* padding: 20px; */
-  /* background: red; */
   line-height: 1;
-  /* border-radius: 4px; */
   @media (max-width: 800px) {
     font-size: 18px;
     padding: 10px;
